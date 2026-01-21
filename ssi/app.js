135c49d8cfbdcb659ccb83fe98e800e7f42a344a
prozy5 <deondrechambers284@gmail.com>
2026-01-21 17:04:30 -0500

    Update app.js

--- ssi/app.js
+++ ssi/app.js
@@ -1,24 +1,9 @@
-const loader = document.getElementById("loader");
-const site = document.getElementById("site");
-const loadingText = document.getElementById("loading-text");
+window.onload = () => {
+  const loader = document.getElementById("loader");
+  const site = document.getElementById("site");
 
-const messages = [
-  "Initializing SS:I systems...",
-  "Establishing secure connection...",
-  "Decrypting archives...",
-  "Loading case database...",
-  "Access granted."
-];
-
-let index = 0;
-
-const interval = setInterval(() => {
-  if (index < messages.length) {
-    loadingText.textContent = messages[index];
-    index++;
-  } else {
-    clearInterval(interval);
+  setTimeout(() => {
     loader.style.display = "none";
     site.classList.remove("hidden");
-  }
-}, 900);
+  }, 2500);
+};
